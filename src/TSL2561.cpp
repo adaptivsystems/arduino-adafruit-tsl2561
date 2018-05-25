02f27afaa637b4ddbacc834d4d8f86ca595eda84
Jean BORT <jean.bort@penbase.com>
2018-05-25 22:41:47 +0200

    test 0ul

--- src/TSL2561.cpp
+++ src/TSL2561.cpp
@@ -174,7 +174,7 @@ uint32_t TSL2561::calculateLux(uint16_t ch0, uint16_t ch1) {
       {b=TSL2561_LUX_B8C; m=TSL2561_LUX_M8C;}
 #else
     unsigned long z = 0ul;
-    if ((ratio >= z) && (ratio <= TSL2561_LUX_K1T)) {
+    if ((ratio >= 0ul) && (ratio <= TSL2561_LUX_K1T)) {
         b = TSL2561_LUX_B1T;
         m = TSL2561_LUX_M1T;
     }
@@ -210,7 +210,7 @@ uint32_t TSL2561::calculateLux(uint16_t ch0, uint16_t ch1) {
 
     unsigned long temp;
     temp = ((channel0 * b) - (channel1 * m));
-    
+
     // do not allow negative lux value
     if (temp < z) temp = 0;
 
